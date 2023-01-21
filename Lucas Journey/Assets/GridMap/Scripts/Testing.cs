681449b6b1215588d71ee6a02c61955888ba0a94
Sebastian Alpizar <sebas_alpizar@hotmail.com>
2023-01-21 15:15:16 -0600

    no message

---
+++ Lucas Journey/Assets/GridMap/Scripts/Testing.cs
@@ -0,0 +1,53 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour {
+
+    private Grid grid;
+    private float mouseMoveTimer;
+    private float mouseMoveTimerMax = .01f;
+
+    private void Start() {
+        grid = new Grid(4, 4, 1f, new Vector3(0, 0));
+
+        //HeatMapVisual heatMapVisual = new HeatMapVisual(grid, GetComponent<MeshFilter>());
+    }
+
+    private void Update() {
+        //HandleClickToModifyGrid();
+        //HandleHeatMapMouseMove();
+
+        // if (Input.GetMouseButtonDown(1)) {
+        //     Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        // }
+    }
+
+    private void HandleClickToModifyGrid() {
+        if (Input.GetMouseButtonDown(0)) {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 1);
+        }
+    }
+
+    private void HandleHeatMapMouseMove() {
+        mouseMoveTimer -= Time.deltaTime;
+        if (mouseMoveTimer < 0f) {
+            mouseMoveTimer += mouseMoveTimerMax;
+            int gridValue = grid.GetValue(UtilsClass.GetMouseWorldPosition());
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), gridValue + 1);
+        }
+    }
+}
